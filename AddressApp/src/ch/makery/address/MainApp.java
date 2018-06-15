48e5ce2cd505cc0a3aa7edb0c5edbb19185dd8bc
r1-21 <r1-21@JeongKS>
2018-06-15 16:30:29 +0900

    06-15

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -16,140 +16,137 @@ import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
 
 public class MainApp extends Application {
-	// ... 다른 변수들 ...
-	private Stage primaryStage;
-	private BorderPane rootLayout;
 
-	/**
-	 * 연락처에 대한 observable 리스트
-	 */
-	private ObservableList<Person> personData = FXCollections.observableArrayList();
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    /**
+     * The data as an observable list of Persons.
+     */
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
-	/**
-	 * 생성자
-	 */
-	public MainApp() {
-		// 샘플 데이터를 추가한다
-		personData.add(new Person("Hans", "Muster"));
-		personData.add(new Person("Ruth", "Mueller"));
-		personData.add(new Person("Heinz", "Kurz"));
-		personData.add(new Person("Cornelia", "Meier"));
-		personData.add(new Person("Werner", "Meyer"));
-		personData.add(new Person("Lydia", "Kunz"));
-		personData.add(new Person("Anna", "Best"));
-		personData.add(new Person("Stefan", "Meier"));
-		personData.add(new Person("Martin", "Mueller"));
-	}
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
 
-	/**
-	 * 연락처에 대한 observable 리스트를 반환한다.
-	 * @return
-	 */
-	public ObservableList<Person> getPersonData() {
-		return personData;
-	}
+    /**
+     * Returns the data as an observable list of Persons. 
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
-	// ... 클래스의 나머지 부분 ...
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
 
-	@Override
-	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("AddressApp");
+        initRootLayout();
 
-		initRootLayout();
+        showPersonOverview();
+    }
 
-		showPersonOverview();
-	}
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
 
-	/**
-	 * 상위 레이아웃을 초기화한다.
-	 */
-	public void initRootLayout() {
-		try {
-			// fxml 파일에서 상위 레이아웃을 가져온다.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-			rootLayout = (BorderPane) loader.load();
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-			// 상위 레이아웃을 포함하는 scene을 보여준다.
-			Scene scene = new Scene(rootLayout);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
 
-	/**
-	 * 상위 레이아웃 안에 연락처 요약(person overview)을 보여준다.
-	 */
-	public void showPersonOverview() {
-		try {
-			// 연락처 요약을 가져온다.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-			AnchorPane personOverview = (AnchorPane) loader.load();
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
 
-			// 연락처 요약을 상위 레이아웃 가운데로 설정한다.
-			rootLayout.setCenter(personOverview);
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
 
-			// 메인 애플리케이션이 컨트롤러를 이용할 수 있게 한다.
-			PersonOverviewController controller = loader.getController();
-			controller.setMainApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     * 
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
 
-	/**
-	 * person의 자세한 정보를 변경하기 위해 다이얼로그를 연다. 만일 사용자가 OK를 클릭하면 주어진 person에 내용을 저장한 후
-	 * true를 반환한다.
-	 *
-	 * @param person
-	 *            the person object to be edited
-	 * @return true if the user clicked OK, false otherwise.
-	 */
-	public boolean showPersonEditDialog(Person person) {
-		try {
-			// fxml 파일을 로드하고 나서 새로운 스테이지를 만든다.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
-			AnchorPane page = (AnchorPane) loader.load();
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
 
-			// 다이얼로그 스테이지를 만든다.
-			Stage dialogStage = new Stage();
-			dialogStage.setTitle("Edit Person");
-			dialogStage.initModality(Modality.WINDOW_MODAL);
-			dialogStage.initOwner(primaryStage);
-			Scene scene = new Scene(page);
-			dialogStage.setScene(scene);
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
 
-			// person을 컨트롤러에 설정한다.
-			PersonEditDialogController controller = loader.getController();
-			controller.setDialogStage(dialogStage);
-			controller.setPerson(person);
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-			// 다이얼로그를 보여주고 사용자가 닫을 때까지 기다린다.
-			dialogStage.showAndWait();
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
-			return controller.isOkClicked();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	/**
-	 * 메인 스테이지를 반환한다.
-	 * 
-	 * @return
-	 */
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
