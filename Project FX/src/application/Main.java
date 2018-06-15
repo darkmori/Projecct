9a2dd8bd8c6f75908cf56d86952aa8ad0de64a0a
r1-21 <r1-21@JeongKS>
2018-06-15 15:33:16 +0900

    06-15

--- Project FX/src/application/Main.java
+++ Project FX/src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("출결관리 프로그램");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
