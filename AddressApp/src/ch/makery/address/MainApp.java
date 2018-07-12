921dd610bcc35e2f10f159868e5bec3cdb3a9dce
r1-21 <r1-21@JeongKS>
2018-07-12 15:17:39 +0900

    07-12

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -15,256 +15,290 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import ch.makery.address.model.Person;
 import ch.makery.address.model.PersonListWrapper;
+import ch.makery.address.view.BirthdayStatisticsController;
 import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
 import ch.makery.address.view.RootLayoutController;
 
 public class MainApp extends Application {
 
-    private Stage primaryStage;
-    private BorderPane rootLayout;
-    
-    /**
-     * The data as an observable list of Persons.
-     */
-    private ObservableList<Person> personData = FXCollections.observableArrayList();
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 
-    /**
-     * Constructor
-     */
-    public MainApp() {
-        // Add some sample data
-        personData.add(new Person("Hans", "Muster"));
-        personData.add(new Person("Ruth", "Mueller"));
-        personData.add(new Person("Heinz", "Kurz"));
-        personData.add(new Person("Cornelia", "Meier"));
-        personData.add(new Person("Werner", "Meyer"));
-        personData.add(new Person("Lydia", "Kunz"));
-        personData.add(new Person("Anna", "Best"));
-        personData.add(new Person("Stefan", "Meier"));
-        personData.add(new Person("Martin", "Mueller"));
-    }
+	/**
+	 * The data as an observable list of Persons.
+	 */
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
 
-    /**
-     * Returns the data as an observable list of Persons. 
-     * @return
-     */
-    public ObservableList<Person> getPersonData() {
-        return personData;
-    }
+	/**
+	 * Constructor
+	 */
+	public MainApp() {
+		// Add some sample data
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+	/**
+	 * Returns the data as an observable list of Persons.
+	 * 
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
 
-        initRootLayout();
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
 
-        showPersonOverview();
-    }
+		// 애플리케이션 아이콘을 설정한다.
+		this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
-    /**
-     * 상위 레이아웃을 초기화하고 마지막으로 열었던 연락처 파일을 가져온다.
-     */
-    public void initRootLayout() {
-        try {
-            // fxml 파일에서 상위 레이아웃을 가져온다.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class
-                    .getResource("view/RootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
+		initRootLayout();
 
-            // 상위 레이아웃을 포함하는 scene을 보여준다.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
+		showPersonOverview();
+	}
 
-            // 컨트롤러한테 MainApp 접근 권한을 준다.
-            RootLayoutController controller = loader.getController();
-            controller.setMainApp(this);
+	/**
+	 * 상위 레이아웃을 초기화하고 마지막으로 열었던 연락처 파일을 가져온다.
+	 */
+	public void initRootLayout() {
+		try {
+			// fxml 파일에서 상위 레이아웃을 가져온다.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// 상위 레이아웃을 포함하는 scene을 보여준다.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
 
-        // 마지막으로 열었던 연락처 파일을 가져온다.
-        File file = getPersonFilePath();
-        if (file != null) {
-            loadPersonDataFromFile(file);
-        }
-    }
+			// 컨트롤러한테 MainApp 접근 권한을 준다.
+			RootLayoutController controller = loader.getController();
+			controller.setMainApp(this);
 
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showPersonOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-            AnchorPane personOverview = (AnchorPane) loader.load();
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-            // Set person overview into the center of root layout.
-            rootLayout.setCenter(personOverview);
+		// 마지막으로 열었던 연락처 파일을 가져온다.
+		File file = getPersonFilePath();
+		if (file != null) {
+			loadPersonDataFromFile(file);
+		}
+	}
 
-            // Give the controller access to the main app.
-            PersonOverviewController controller = loader.getController();
-            controller.setMainApp(this);
+	/**
+	 * Shows the person overview inside the root layout.
+	 */
+	public void showPersonOverview() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    /**
-     * Opens a dialog to edit details for the specified person. If the user
-     * clicks OK, the changes are saved into the provided person object and true
-     * is returned.
-     * 
-     * @param person the person object to be edited
-     * @return true if the user clicked OK, false otherwise.
-     */
-    public boolean showPersonEditDialog(Person person) {
-        try {
-            // Load the fxml file and create a new stage for the popup dialog.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
-            AnchorPane page = (AnchorPane) loader.load();
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(personOverview);
 
-            // Create the dialog Stage.
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Edit Person");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.initOwner(primaryStage);
-            Scene scene = new Scene(page);
-            dialogStage.setScene(scene);
+			// Give the controller access to the main app.
+			PersonOverviewController controller = loader.getController();
+			controller.setMainApp(this);
 
-            // Set the person into the controller.
-            PersonEditDialogController controller = loader.getController();
-            controller.setDialogStage(dialogStage);
-            controller.setPerson(person);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-            // Show the dialog and wait until the user closes it
-            dialogStage.showAndWait();
+	/**
+	 * Opens a dialog to edit details for the specified person. If the user clicks
+	 * OK, the changes are saved into the provided person object and true is
+	 * returned.
+	 * 
+	 * @param person
+	 *            the person object to be edited
+	 * @return true if the user clicked OK, false otherwise.
+	 */
+	public boolean showPersonEditDialog(Person person) {
+		try {
+			// Load the fxml file and create a new stage for the popup dialog.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
 
-            return controller.isOkClicked();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+			// Create the dialog Stage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Person");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
 
-    /**
-     * Returns the main stage.
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
+			// Set the person into the controller.
+			PersonEditDialogController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setPerson(person);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-    /**
-     * 연락처 파일 환경설정을 반환한다.
-     * 즉 파일은 마지막으로 열린 것이고, 환경설정은 OS 특정 레지스트리로부터 읽는다.
-     * 만일 preference를 찾지 못하면 null을 반환한다.
-     *
-     * @return
-     */
-    public File getPersonFilePath() {
-        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
-        String filePath = prefs.get("filePath", null);
-        if (filePath != null) {
-            return new File(filePath);
-        } else {
-            return null;
-        }
-    }
+			// Show the dialog and wait until the user closes it
+			dialogStage.showAndWait();
 
-    /**
-     * 현재 불러온 파일의 경로를 설정한다. 이 경로는 OS 특정 레지스트리에 저장된다.
-     *
-     * @param file the file or null to remove the path
-     */
-    public void setPersonFilePath(File file) {
-        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
-        if (file != null) {
-            prefs.put("filePath", file.getPath());
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-            // Stage 타이틀을 업데이트한다.
-            primaryStage.setTitle("AddressApp - " + file.getName());
-        } else {
-            prefs.remove("filePath");
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
 
-            // Stage 타이틀을 업데이트한다.
-            primaryStage.setTitle("AddressApp");
-        }
-    }
-    /**
-     * 지정한 파일로부터 연락처 데이터를 가져온다. 현재 연락처 데이터로 대체된다.
-     *
-     * @param file
-     */
-    public void loadPersonDataFromFile(File file) {
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(PersonListWrapper.class);
-            Unmarshaller um = context.createUnmarshaller();
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-            // 파일로부터 XML을 읽은 다음 역 마샬링한다.
-            PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+	/**
+	 * 연락처 파일 환경설정을 반환한다. 즉 파일은 마지막으로 열린 것이고, 환경설정은 OS 특정 레지스트리로부터 읽는다. 만일
+	 * preference를 찾지 못하면 null을 반환한다.
+	 *
+	 * @return
+	 */
+	public File getPersonFilePath() {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		String filePath = prefs.get("filePath", null);
+		if (filePath != null) {
+			return new File(filePath);
+		} else {
+			return null;
+		}
+	}
 
-            personData.clear();
-            personData.addAll(wrapper.getPersons());
+	/**
+	 * 현재 불러온 파일의 경로를 설정한다. 이 경로는 OS 특정 레지스트리에 저장된다.
+	 *
+	 * @param file
+	 *            the file or null to remove the path
+	 */
+	public void setPersonFilePath(File file) {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		if (file != null) {
+			prefs.put("filePath", file.getPath());
 
-            // 파일 경로를 레지스트리에 저장한다.
-            setPersonFilePath(file);
+			// Stage 타이틀을 업데이트한다.
+			primaryStage.setTitle("AddressApp - " + file.getName());
+		} else {
+			prefs.remove("filePath");
 
-        } catch (Exception e) { // 예외를 잡는다
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Could not load data");
-            alert.setContentText("Could not load data from file:\n" + file.getPath());
+			// Stage 타이틀을 업데이트한다.
+			primaryStage.setTitle("AddressApp");
+		}
+	}
 
-            alert.showAndWait();
-        }
-    }
+	/**
+	 * 지정한 파일로부터 연락처 데이터를 가져온다. 현재 연락처 데이터로 대체된다.
+	 *
+	 * @param file
+	 */
+	public void loadPersonDataFromFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
 
-    /**
-     * 현재 연락처 데이터를 지정한 파일에 저장한다.
-     *
-     * @param file
-     */
-    public void savePersonDataToFile(File file) {
-        try {
-            JAXBContext context = JAXBContext
-                    .newInstance(PersonListWrapper.class);
-            Marshaller m = context.createMarshaller();
-            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			// 파일로부터 XML을 읽은 다음 역 마샬링한다.
+			PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
 
-            // 연락처 데이터를 감싼다.
-            PersonListWrapper wrapper = new PersonListWrapper();
-            wrapper.setPersons(personData);
+			personData.clear();
+			personData.addAll(wrapper.getPersons());
 
-            // 마샬링 후 XML을 파일에 저장한다.
-            m.marshal(wrapper, file);
+			// 파일 경로를 레지스트리에 저장한다.
+			setPersonFilePath(file);
 
-            // 파일 경로를 레지스트리에 저장한다.
-            setPersonFilePath(file);
-        } catch (Exception e) { // 예외를 잡는다.
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Could not save data");
-            alert.setContentText("Could not save data to file:\n" + file.getPath());
+		} catch (Exception e) { // 예외를 잡는다
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Could not load data");
+			alert.setContentText("Could not load data from file:\n" + file.getPath());
 
-            alert.showAndWait();
-        }
-    }
+			alert.showAndWait();
+		}
+	}
+
+	/**
+	 * 현재 연락처 데이터를 지정한 파일에 저장한다.
+	 *
+	 * @param file
+	 */
+	public void savePersonDataToFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			// 연락처 데이터를 감싼다.
+			PersonListWrapper wrapper = new PersonListWrapper();
+			wrapper.setPersons(personData);
+
+			// 마샬링 후 XML을 파일에 저장한다.
+			m.marshal(wrapper, file);
+
+			// 파일 경로를 레지스트리에 저장한다.
+			setPersonFilePath(file);
+		} catch (Exception e) { // 예외를 잡는다.
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Could not save data");
+			alert.setContentText("Could not save data to file:\n" + file.getPath());
+
+			alert.showAndWait();
+		}
+	}
+
+	/**
+	 * 생일 통계를 보여주기 위해 다이얼로그를 연다.
+	 */
+	public void showBirthdayStatistics() {
+		try {
+			// FXML 파일을 불러와서 팝업의 새로운 Stage를 만든다.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/BirthdayStatistics.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Birthday Statistics");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+
+			// 연락처를 컨트롤러에 설정한다.
+			BirthdayStatisticsController controller = loader.getController();
+			controller.setPersonData(personData);
+
+			dialogStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
